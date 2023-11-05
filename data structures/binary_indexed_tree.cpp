fdc466be8a26bdc8902c648f9960103d4b046d6d
hazzlerr <127759611+hazzlerr@users.noreply.github.com>
2023-11-05 15:59:13 -0500

    Update binary_indexed_tree.cpp

--- data structures/binary_indexed_tree.cpp
+++ data structures/binary_indexed_tree.cpp
@@ -2,14 +2,15 @@ template <typename T>
 struct bit{
       int n;
       vec <T> t;
-      bit(int z): n(z + 5), t(z + 5) {}
+      T ielement;
+      bit(int z, const T &ielement): n(z + 5), ielement(ielement), t(z + 5, ielement) {}
       void upd(int p, T x){
             for (p += 3; p < n; p += p & -p){
                   t[p] += x;
             }
       }
       T get(int r){
-            T res = 0;
+            T res = ielement;
             for (r += 3; r > 0; r -= r & -r){
                   res += t[r];
             }
